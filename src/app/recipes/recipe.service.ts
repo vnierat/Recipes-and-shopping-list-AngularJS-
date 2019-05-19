774a0585574e831e9093770ac171aaf6299ed4f0
vnierat <vnierat@hotmail.fr>
2019-05-19 13:12:31 +0200

    To recipe => to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,20 +1,24 @@
+
+import { EventEmitter, Injectable } from '@angular/core';
+
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
 import { Ingredient } from './../shared/ingredient.model';
-import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
+@Injectable()
 export class RecipeService {
     recipeSelected  = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe(
-            'A test Recipe', 
-            'This is a simply test', 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/user-collections/my-colelction-image/2015/12/recipe-image-legacy-id--195570_11.jpg?itok=r-dZNGTt',
+            'Indian Meal', 
+            'Curry chicken', 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/user-collections/my-colelction-image/2015/12/recipe-image-legacy-id--195570_11.jpg?itok=r-dZNGTt',
             [
                 new Ingredient('Meat', 1),
                 new Ingredient('Avocado', 3)
             ]),
         new Recipe('Another test Recipe', 
-        'This is a simply test', 
+        'This is a tasty kebab', 
         'https://static.cuisineaz.com/240x192/i108058-kebab-sans-gluten.jpg',
         [
             new Ingredient('Bread', 1),
@@ -22,7 +26,15 @@ export class RecipeService {
         ])
       ];
 
+      constructor(private slService: ShoppingListService) {
+
+      }
+
       getRecipes() {
           return this.recipes.slice();
       } 
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+      }
 }
